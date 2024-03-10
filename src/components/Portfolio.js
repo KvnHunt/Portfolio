c3eb6bdf7b91a63dce08bae30c06a1478ddf2cb4
KvnHunt <huntk.twitch@gmail.com>
2024-03-10 01:30:58 -0500

    Redirecting

---
+++ src/components/Portfolio.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Project from "./Project";
+
+const projects = [
+    {
+        id: 0,
+        title: ,
+        image: "/img/pocket-traders.png",
+        description: "Allows you to trade pokemon and create a team. I did the design and front end!",
+        repo: "https://github.com/KvnHunt/ChallTexties",
+        live: "",
+      },
+      {
+        id: 1,
+        title: ,
+        image: "/img/techtopics.png",
+        description: "Allows you to edit and post blogs. You can also leave comments on your own blog or the blogs of others.",
+        repo: "https://github.com/KvnHunt/RegexMone",
+        live: 
+      },
+      {
+        id: 2,
+        title: ,
+        image: "/img/",
+        description: "Allows instructors to quickly create quizzes and print the quiz to a PDF to distribute to their students, as well as keep a copy for themselves with the answer key!",
+        repo: "https://github.com/KvnHunt/NOTREADME",
+        live: "https://drive.google.com/file/d/1MO9qJJG4mc59Ky1J_RELkzGCHt9CR6ou/view",
+        
+      },
+
+      {
+        id: 3,
+        title: "Flavor Trail",
+        image: "/img/",
+        description: "Allows instructors to quickly create quizzes and print the quiz to a PDF to distribute to their students, as well as keep a copy for themselves with the answer key!",
+        repo: "https://github.com/Shawki45/Flavor-Trail.git",
+        live: "https://drive.google.com/file/d/1MO9qJJG4mc59Ky1J_RELkzGCHt9CR6ou/view",
+        
+      },
+
+    ]
